d1caa5becfd2e1b8f9b29175bf343d4f2b2fe4cf
omarmoharrm <20100@Omar.(none)>
2023-08-29 18:29:29 +0300

    second

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 initWebRoutes(app)
-let port = process.env.port || 8080
+let port = process.env.PORT
 app.listen(port, () => {
   console.log("app listening on port: " + port)
 })
