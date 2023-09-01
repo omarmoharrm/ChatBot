2893833a64ccc782c5562ffc0df10808ae557f80
omarmoharrm <20100@Omar.(none)>
2023-09-01 17:55:27 +0300

    update2

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -63,10 +63,10 @@ function handleMessage(sender_psid, received_message) {
 
   // Check if the message contains text
   if (received_message.text === "بكام") {
-    console.log(typeof(received_message.text))
+    console.log(typeof received_message.text)
     // Create the payload for a basic text message
     response = {
-      text: ``,
+      text: `ممكن تبعتلنا صورة المنتج المطلوب`,
     }
   } else if (received_message.attachments) {
     // Gets the URL of the message attachment
