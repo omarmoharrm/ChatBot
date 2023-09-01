a495491975febcfdca2fdef954ac7ec09b679744
omarmoharrm <20100@Omar.(none)>
2023-09-01 17:51:10 +0300

    update1

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -62,8 +62,8 @@ function handleMessage(sender_psid, received_message) {
   let response
 
   // Check if the message contains text
-  if (received_message.text) {
-    console.log(received_message.text)
+  if (received_message.text === "بكام") {
+    console.log(typeof(received_message.text))
     // Create the payload for a basic text message
     response = {
       text: ``,
